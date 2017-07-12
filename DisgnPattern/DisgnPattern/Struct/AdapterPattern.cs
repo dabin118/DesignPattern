ac3a0f3be7d37c44adfd8b55056d5adc69032228
HuangWenbin <pjhuangwenbin@163.com>
2017-07-12 21:36:16 +0800

    add 适配器模式

---
+++ DisgnPattern/DisgnPattern/Struct/AdapterPattern.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisgnPattern.Struct
+{
+    class AdapterPattern
+    {
+    }
+    //适配器模式：
+    //使得新环境中不需要去重复实现已经存在了的实现而很好地把现有对象（指原来环境中的现有对象）加入到新环境来使用。
+
+
+
+
+
+}
