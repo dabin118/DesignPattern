f9b6568af68b85ed526361c13efd43e0fc7527ad
HuangWenbin <pjhuangwenbin@163.com>
2017-07-26 23:40:51 +0800

    代码优化

--- DisgnPattern/DisgnPattern/Product/PrototypePattern.cs
+++ DisgnPattern/DisgnPattern/Product/PrototypePattern.cs
@@ -8,6 +8,26 @@ namespace DisgnPattern.Product
 {
 
     //原型模式:
+    public class PrototypePattern
+    {
+        public static void Test()
+        {
+            MonkeyKingPrototype prototypeMonkeyKing = new ConcretePrototype("MonkeyKing");
+
+            // 变一个
+            MonkeyKingPrototype cloneMonkeyKing = prototypeMonkeyKing.Clone() as ConcretePrototype;
+            Console.WriteLine("Cloned1:\t" + cloneMonkeyKing.Id);
+
+
+
+            // 变两个
+            MonkeyKingPrototype cloneMonkeyKing2 = prototypeMonkeyKing.Clone() as ConcretePrototype;
+            Console.WriteLine("Cloned2:\t" + cloneMonkeyKing2.Id);
+            Console.ReadLine();
+        }
+    }
+
+    //原型模式:
     //每个类实例都是相同的，当我们需要多个相同的类实例时，没必要每次都使用new运算符去创建相同的类实例对象
     //此时我们一般思路就是想——只创建一个类实例对象，
     //如果后面需要更多这样的实例，可以通过对原来对象拷贝一份来完成创建，这样在内存中不需要创建多个相同的类实例，从而减少内存的消耗和达到类实例的复用。
